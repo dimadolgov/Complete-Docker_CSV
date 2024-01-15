02f392d280df3cb54d216334f9327d32de945891
Dima Dolgov <Dima@dolgov.co.il>
2024-01-15 21:04:24 +0200

    Initial

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-# Dockerfile
 FROM python:3.8-slim
 
 WORKDIR /app
 
 COPY requirements.txt /app/requirements.txt
+
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . /app
--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, send_file
 import pandas as pd
 import os
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='web')
 
 @app.route('/')
 def index():
